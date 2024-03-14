b17dd2a5b4f2938bae777376d5b640d20b275c56
Yauhen Yavorski <yauhen.yavorski@gmail.com>
2024-03-14 22:00:54 +0100

    📝 Add parserList for different formats

--- utils/parseList.ts
+++ utils/parseList.ts
@@ -5,6 +5,14 @@ import { Format, FormatInstanceList } from "@/types/Format";
 
 const availableFormats: Format[] = ["Fasta", "FASTQ", "Clustal"];
 
+/**
+ * @description A list of parsers for different formats
+ * @type {Record<Format, (input: string) => FormatInstanceList>}
+ * @property {Format} - The format of the input
+ * @property {(input: string) => FormatInstanceList} - The parser function
+ * @returns {FormatInstanceList} - The list of instances of the provided format
+ * @example parserList["Fasta"](">")
+ */
 const parserList: Record<Format, (input: string) => FormatInstanceList> = {
   Fasta: FastaParser.parseMultiple,
   FASTQ: FastqParser.parseMultiple,
